7de66cae29e9ff13773a98422d59f269b174fda8
Oseas Oliveira <ozzyoliv@gmail.com>
2024-08-08 15:12:45 -0300

    feat: a add Comments associations between User e Post models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates :body, presence: true, length: { maximum: 500 }
+  validates :post_id, presence: true
+  validates :user_id, presence: true
 end
